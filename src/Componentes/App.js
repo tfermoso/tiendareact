416795bbb81d63ef2c3c674ad95b16e778a63120
tfermoso <tomas.fermoso@gmail.com>
2023-04-18 15:41:44 +0200

    tienda react primer commit

---
+++ src/Componentes/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Producto from './Producto';
+
+function App() {
+  return (
+    <div className="App">
+     
+     
+       <Producto/>
+     
+    </div>
+  );
+}
+
+export default App;
