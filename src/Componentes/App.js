75344019a5c2550b71ed9d0ede4fc6ef79ea7230
tfermoso <tomas.fermoso@gmail.com>
2023-04-18 18:40:01 +0200

    creando array de productos

--- src/Componentes/App.js
+++ src/Componentes/App.js
@@ -7,12 +7,28 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      'p': {
+      'productos': [{
+        id:1,
+        nombre: "Learn PHP 7",
+        imagen: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg",
+        descripcion: "This new book on PHP 7 introduces writing solid, secure, object-oriented code in the new PHP 7: you will create a complete three-tier application using a natural process of building and testing modules within each tier. This practical approach teaches you about app development and introduces PHP features when they are actually needed rather than providing you with abstract theory and contrived examples.",
+        precio: 300
+      },
+      {
+        id:2,
+        nombre: "Learn PHP 7",
+        imagen: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg",
+        descripcion: "This new book on PHP 7 introduces writing solid, secure, object-oriented code in the new PHP 7: you will create a complete three-tier application using a natural process of building and testing modules within each tier. This practical approach teaches you about app development and introduces PHP features when they are actually needed rather than providing you with abstract theory and contrived examples.",
+        precio: 300
+      },
+      {
+        id:3,
         nombre: "Learn PHP 7",
         imagen: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg",
         descripcion: "This new book on PHP 7 introduces writing solid, secure, object-oriented code in the new PHP 7: you will create a complete three-tier application using a natural process of building and testing modules within each tier. This practical approach teaches you about app development and introduces PHP features when they are actually needed rather than providing you with abstract theory and contrived examples.",
         precio: 300
       }
+    ]
 
     };
   }
