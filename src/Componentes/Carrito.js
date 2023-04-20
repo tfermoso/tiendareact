9a6c578987de5f91bb20e3a71abcb467a3b1e866
tfermoso <tomas.fermoso@gmail.com>
2023-04-20 18:37:53 +0200

    mostrando formulario

--- src/Componentes/Carrito.js
+++ src/Componentes/Carrito.js
@@ -1,7 +1,7 @@
 function Carrito(props) {
     let total = 0;
-    let novacio=props.cart.length!==0;
-    props.cart.map(p=>total+=parseFloat(p.cantidad*p.precio));
+
+    props.cart.map(p => total += parseFloat(p.cantidad * p.precio));
     return (
         <div>
             <h3>Lista del carrito</h3>
@@ -16,19 +16,19 @@ function Carrito(props) {
                     </tr>
                 </thead>
                 <tbody>
-                    {  
-                                
+                    {
+
                         props.cart.map((p) => (
-                        <tr>
-                            <th scope="row">{p.nombre}</th>
-                            <td>{p.cantidad}</td>
-                            <td>{p.precio}</td>
-                            <td>{p.cantidad * p.precio}€</td>
-                            <td>
-                            <button onClick={()=>{props.eliminarProducto(p)}} class="btn btn-danger" type="submit" name="btnAccion" value="Eliminar">Eliminar</button>
-                            </td>
-                        </tr>
-                    ))
+                            <tr>
+                                <th scope="row">{p.nombre}</th>
+                                <td>{p.cantidad}</td>
+                                <td>{p.precio}</td>
+                                <td>{p.cantidad * p.precio}€</td>
+                                <td>
+                                    <button onClick={() => { props.eliminarProducto(p) }} class="btn btn-danger" type="submit" name="btnAccion" value="Eliminar">Eliminar</button>
+                                </td>
+                            </tr>
+                        ))
                     }
                     <tr>
                         <td colSpan={2} align="right">
@@ -39,25 +39,25 @@ function Carrito(props) {
                         </td>
                         <td></td>
                     </tr>
-                    
-                    {novacio && (<tr>
-                    <td colSpan={5}>
-                        <form onSubmit={(e)=>{e.preventDefault(); props.pagar(document.getElementById("email").value)}}  action="">
-                            <div class="alert alert-success" role="alert">
-                                <div class="mb-3">
-                                    <label for="" class="form-label">Email</label>
-                                    <input type="email" name="email" id="email" class="form-control" placeholder="Por favor escribe tu correo" aria-describedby="helpId" required/>
-                                    <small id="helpId" class="text-muted">Los productos se enviarán a este email</small>
+
+                    {props.cart.length !== 0 ? (<tr>
+                        <td colSpan={5}>
+                            <form onSubmit={(e) => { e.preventDefault(); props.pagar(document.getElementById("email").value) }} action="">
+                                <div class="alert alert-success" role="alert">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Email</label>
+                                        <input type="email" name="email" id="email" class="form-control" placeholder="Por favor escribe tu correo" aria-describedby="helpId" required />
+                                        <small id="helpId" class="text-muted">Los productos se enviarán a este email</small>
+                                    </div>
+                                    <div class="d-grid gap-2">
+                                        <button type="submit" name="btnAccion" value="pagar" id="" class="btn btn-primary btn-lg btn-block">Proceder a pagar {'>>'}</button>
+                                    </div>
                                 </div>
-                                <div class="d-grid gap-2">
-                                  <button type="submit" name="btnAccion" value="pagar" id="" class="btn btn-primary btn-lg btn-block">Proceder a pagar {'>>'}</button>
-                                </div>
-                            </div>
 
 
-                        </form>
-                    </td>
-                </tr>)}
+                            </form>
+                        </td>
+                    </tr>) : null}
                 </tbody>
             </table>
 
